ba2ec4c1a18d324e9586e19ba5ca5ddcdfa7f4ad
Eduardo Rocha <eduardorocha460@gmail.com>
2017-09-17 22:25:35 -0300

    adicionando algum tipo de config no content

--- Content.js
+++ Content.js
@@ -16,8 +16,46 @@ var Content = React.createClass ({
       autoFocus= { false }
       fetchDetails= { true }
       onPress = { (data, details = null) => { // os 'details' são fornecidos quando fetchDetails for verdadeiro
-              
+          console.log(data);
+          console.log(details);
       }}
+      getDefaultValue = {() => {
+        return ''; //  valor padrao de entrada de texto
+      }}
+      query = {{
+        // minha key do google
+        key: 'AIzaSyBpoEDWF6LYz_vA3nwwcMZ_P8ZaLSbTSVc',
+        language: 'pt-br',
+        types: '(cities)',
+      }}
+      styles = {{
+        description: {
+          fontWeight: 'bold',
+        },
+      predefinedPlacesDescription: {
+        color: '#e31414',
+      },
+      }}
+
+      currentLocation = { true } // adicionando um botao atual na parte superior da lista de lugares
+      currentLocationLabel = "Current Location"
+      nearbyPlacesAPI = 'GooglePlacesSearch'
+      GoogleReverseGeocodingQuery = {{
+
+      }}
+
+      GooglePlacesSearchQuery = {{
+
+        rank: 'distance',
+        types: 'food',
+      }}
+
+      filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+
+      predefinedPlaces={[homePlace, workPlace]}
+
+      predefinedPlacesAlwaysVisible={true}
+
     )
   }
 })
